a6a67ca6ff400065f9e22f1b2ef0ca887e416c22
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2023-07-17 09:31:38 +0100

    n

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -82,6 +82,7 @@
         <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
         <!-- Vendor JS Files -->
+        <script src="{{ asset('assets/vendor/jquery/js/jquery.min.js') }}"></script>
         <script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
         <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
         <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
@@ -91,8 +92,13 @@
         <script src="{{ asset('assets/vendor/typed.js/typed.umd.js') }}"></script>
         <script src="{{ asset('assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
         <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
+        <script src="{{ asset('assets/vendor/autosize/js/autosize.min.js') }}"></script>
 
-        <!-- Template Main JS File -->
+        <!-- Main JS File -->
         <script src="{{ asset('assets/js/main.js') }}"></script>
+
+        <script type="text/javascript">
+            autosize($('textarea'));
+        </script>
     </body>
 </html>
